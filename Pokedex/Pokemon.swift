43b6f63f29829d1a7dad2c93274bd5a66e984b6a
Tk <timkaldybek@gmail.com>
2020-11-23 21:29:36 +0600

    Minor update

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Timur on 11/20/20.
+//
+
+import Foundation
+
+struct Pokemon{
+    let name: String
+    let number: Int
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    let pokemon = [
+        Pokemon(name: "Bulbasaur", number: 1),
+        Pokemon(name: "Pickachu", number: 2),
+        Pokemon(name: "Raichu", number: 3),
+        Pokemon(name: "Pichu", number: 4),
+        Pokemon(name: "Meow-Meow", number: 5)
+    ]
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.reloadData()
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "id", for: indexPath)
+        cell.textLabel?.text = pokemon[indexPath.row].name
+        return cell
+    }
+ 
 }
+
 
